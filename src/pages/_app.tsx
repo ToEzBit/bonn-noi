e5996c4b242bdfb91dc6a4690f464f730923692b
Jirayut Natrachart <86563611+ToEzBit@users.noreply.github.com>
2023-05-25 11:38:15 +0700

    :poop: test to do to issue (#1)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,4 +8,9 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
 };
 
+// TODO: test todo to issue
+// test description for issue.
+// assignees: toezbit
+// labels: enhancement, help wanted
+
 export default api.withTRPC(MyApp);
